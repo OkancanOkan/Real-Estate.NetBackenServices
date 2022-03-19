43bb7c771fd2a0d9c980478715437d02231baf89
Okancan Okan <okancan.okan@gmail.com>
2022-03-19 18:21:17 +0300

    Add project files.

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,62 @@
+﻿using InsaatAPINet.dao;
+using InsaatAPINet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InsaatAPINet.Controllers
+{
+    [Route("/")]
+    
+    public class ContentController : ControllerBase
+    {
+        Content content = new Content();
+
+        [HttpGet]
+        [Route("getcitylist")]
+        public string GetCityList()
+            {
+                return content.GetCityList();
+            }
+
+            [HttpPost]
+            [Route("PostCity")]
+            [Consumes("application/json")]
+            public async Task<string> PostCity([FromBody]City req)
+             {
+                return await content.PostCity(req);  
+             }
+
+        [HttpPut]
+        [Route("PutCity")]
+        [Consumes("application/json")]
+        public async Task<string> PutCity([FromBody] City req)
+        {
+            return await content.PutCity(req);
+        }
+
+        [HttpDelete]
+        [Route("DeleteCity")]
+        [Consumes("application/json")]
+        public async Task<string> DeleteCity([FromBody] City req)
+        {
+            return await content.DeleteCity(req);
+        }
+        // Get FlatTypeList
+        [HttpGet]
+        [Route("GetFlatTypeList")]
+        public async Task< string> GetFlatTypeList()
+        {
+            return await content.GetFlatTypeList();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ dao/Content.cs
@@ -0,0 +1,103 @@
+﻿using InsaatAPINet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+
+namespace InsaatAPINet.dao
+{
+    public class Content
+    {
+
+        InsaatContext context = new InsaatContext();
+
+        public object DataTime { get; private set; }
+
+        public string GetCityList()
+        {
+            var cities = context.Cities.ToList();
+
+            var resultJson = JsonConvert.SerializeObject(new [] 
+            {
+                new 
+            {
+                Result ="İşlem Başarılı!.",
+                Message= "",
+                CityTable = cities,
+            }
+            });
+            return resultJson;
+        }
+
+        public async Task<string> PostCity([FromBody]City req)
+        {
+            var cities = new City
+            {
+                CityName = req.CityName,
+
+            };
+            context.Cities.Add(cities);
+            await context.SaveChangesAsync();
+            var resultJson = JsonConvert.SerializeObject(new
+            {
+                Result = "İşlem Başarılı!",
+            });
+            return resultJson;
+        }
+        public async Task<string>PutCity([FromBody] City req)
+        {
+            var city = context.Cities.FirstOrDefault(c=> c.CityID == req.CityID);
+            city.CityName = req.CityName;
+            city.UpdateDate = DateTime.Now;
+            await context.SaveChangesAsync();
+            var resultJson = JsonConvert.SerializeObject(new
+            {
+                Result = "İşlem Başarılı",
+            });
+            return resultJson;
+        }
+
+        public async Task<string> DeleteCity([FromBody] City req)
+        {
+            var city = context.Cities.FirstOrDefault(c => c.CityID == req.CityID);
+            context.Cities.Remove(city);
+            await context.SaveChangesAsync();
+            var resultJson = JsonConvert.SerializeObject(new
+            {
+                Result = "İşlem Başarılı",
+            });
+            return resultJson;
+        }
+
+        public async Task<string>GetFlatTypeList()
+        {
+            var con = new SqlConnection(@"Server=OKANCAN; Database=Insaat;User ID=sa;Password=1;");
+            con.Open();
+            var query = "SELECT FlatTypeID,FlatTypeName FROM FlatType";
+            var cmd = new SqlCommand(query, con);
+            SqlDataReader reader = await cmd.ExecuteReaderAsync();
+            var dt = new DataTable();
+            dt.Load(reader);
+            List<DataRow> result = dt.AsEnumerable().ToList();
+            reader.Close();
+            con.Close();
+            var resultJson = JsonConvert.SerializeObject(new[]
+            {
+                                (
+                    Result : " İşlem Başarılı!",
+                    FlatTypeTable : result.CopyToDataTable()
+
+                )
+            }); 
+            return resultJson;
+
+        }
+       
+
+    }
+}
